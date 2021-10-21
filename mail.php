7bdd27ae852fa6eb29071a53518bed21a581fbd4
Nikko Dacara <franzdacara25@gmail.com>
2021-10-21 12:19:17 +0800

    Contact

--- mail.php
+++ mail.php
@@ -5,10 +5,11 @@ $email = $_POST ['emailaddress'];
 $message = $_POST['message'];
 
 $to = 'franzdacara25@gmail.com';
+
 $subject = "Mail From website";
 $txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n Message = " . $message ;
 
-$headers = "From: Nikko@personal.com" . "\r\n " . "CC: Sombody@example.com";
+$headers = "From: noreply@yoursite.com" . "\r\n " . "CC: Sombodyelse@example.com";
 if($email != NULL){
     mail($to,$subject,$txt,$headers)
 }
