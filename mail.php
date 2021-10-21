45448e163410ec0cba549283f3926554e23e332f
Nikko Dacara <franzdacara25@gmail.com>
2021-10-21 12:00:56 +0800

    contacts
    Contact Section is done and workable

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$message = $_POST['message'];
+
+$to = 'franzdacara25@gmail.com';
+$subject = "Mail From website";
+$txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n Message = " . $message ;
+
+$headers = "From: Nikko@personal.com" . "\r\n " . "CC: Sombody@example.com";
+if($email != null){
+    mail($to,$subject,$txt,$headers)
+}
+header("Location: thankyou.html");
